0731f7395c245a5c8a71df7fce2c069d695e0688
Shaun Miller <binarypearl@gmail.com>
2014-01-15 20:48:25 -0600

    trying update again

--- instespeak.pl
+++ instespeak.pl
@@ -11,6 +11,10 @@ use strict;
 use Glib qw/TRUE FALSE/;
 use Gtk2 '-init';
 use Gnome2::Wnck;
+use DBI;
+
+# For connection to the java opennlp java instance
+use IO::Socket;
 
 my $cmds;
 
@@ -29,8 +33,24 @@ my $weather_temperature = "";
 my $weather_windchill = "";
 my $previous_command = "";
 
-system ("clear");
+my @opennlp_output = "";
+my $opennlp_record = "";
 
+my $project_dir = "/mnt/projects/speech/apache-opennlp-1.5.3/";
+
+my $socket = "";
+my $pos_tagged_output = "";
+
+my @pos_tags = ();
+my $pos_tag = "";
+
+my $text = "";
+my $part_of_speech = "";
+
+my $dbfile = "/mnt/projects/speech/instespeak/instespeak.db";
+
+
+system ("clear");
 
 if ($initial_run) {
 	print "Initializing: please wait...\n";
@@ -56,12 +76,60 @@ while (<$cmds>) {
 	chomp ($_);
 
 	if ($_ =~ m/^(\d{9})(: )(.*)/) {
-		#print "***$3***\n";
-
 		$command_to_execute = $3;	
+
+		$command_to_execute =~ s/whether/weather/;
 
 		print "***The command that I got was: $command_to_execute***\n";
 		#`echo "$command_to_execute" | festival --tts`;
+
+		#*** New database code here:
+
+		# So first, we need to pass the text to OpenNLP, so we tag the words with the different parts of speech.
+		#@opennlp_output = `echo $command_to_execute | $project_dir/bin/opennlp POSTagger $project_dir/bin/en-pos-maxent.bin`;
+
+		#foreach $opennlp_record (@opennlp_output) {
+		#	chomp ($opennlp_record);
+                # 
+		#	print $opennlp_record . "\n";
+		#}
+
+		# Lets connect to the java socket for opennlp:
+		$socket = IO::Socket::INET->new (
+						PeerAddr => 'localhost',
+						PeerPort => '9999',
+						Proto	=> 'tcp',
+						Type	=> SOCK_STREAM
+						) or print "oops\n";
+
+		# Apparently java wasn't happy with the \n, it wanted the \r to indicate it was the end of the message.
+		# Otherwise in.readLine() was hanging waiting for it.
+		print $socket "text:$command_to_execute\n\r";
+
+		$pos_tagged_output = <$socket>;
+
+		print "Here is the replay back: $pos_tagged_output";
+		
+		# Ok, so now that we got the PosTagged version of our voice command, now we need to parse through it.
+		@pos_tags = split (' ', $pos_tagged_output);
+
+		foreach $pos_tag (@pos_tags) {
+			chomp ($pos_tag);
+			
+			if ($pos_tag =~ m/(.*)(_)(.*)/) {
+				$text = $1;
+				$part_of_speech = $3;
+				
+				if ($part_of_speech eq "NN") {
+					#
+					
+				}
+			}
+		}
+
+		close ($socket);
+
+		#*** End new database code here.
 
 		if ($command_to_execute =~ m/^computer$/) {
 			`echo "What can I do for you?" | festival --tts`;
@@ -108,11 +176,11 @@ while (<$cmds>) {
 				foreach $weather_report_record (@weather_report_output_records) {
 					chomp ($weather_report_record);
 
-					if ($weather_report_record =~ m/(^Temperature: )(.*)(F)(.*)/) {
+					if ($weather_report_record =~ m/(^Temperature: )(.*)( F)(.*)/) {
 						$weather_temperature = $2;
 					} 
 
-					elsif ($weather_report_record =~ m/(^Windchill: )(.*)(F)(.*)/) {
+					elsif ($weather_report_record =~ m/(^Windchill: )(.*)( F)(.*)/) {
 						$weather_windchill = $2;
 					}
 				}
